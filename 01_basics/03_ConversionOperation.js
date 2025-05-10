dcbada886b4a6f89231462ea3c5f6d76fafad749
Lalit42224 <lallipandit8171@gmail.com>
2025-05-10 17:10:19 +0000

    conversion

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33abcd"
+console.log(typeof(score))
+let scoreNumber = Number(score)
+console.log(scoreNumber)
+let isLogged  = true
+let numberbool = Number(isLogged)
+console.log(typeof(numberbool));
+console.log(numberbool);
+// "33" => 33
+// true =>1;
+// false=>0;
+// "33acvb" => NaN(Not a nUMBER)
