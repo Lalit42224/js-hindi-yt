b1f068ae8a9eb5f7183328370a246addb0eb057c
Lalit42224 <lallipandit8171@gmail.com>
2025-05-10 16:14:55 +0000

    variables h ye

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId =  144553
+let accountEmail = "lalit@gmail.com"
+var accountPassword = "1234"
+let accountState;
+// accountId = 2// not allowed
+/*
+Perfer not use var
+beacuse of issue in block scope and functional block
+*/
+accountCity = "Delhi"
+console.log(accountId );
+accountEmail = "hahahao"
+accountPassword="234123134"
+accountCity =  "Aligarh"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+ 
