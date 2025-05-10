425e9ff23f771f26b0a09407380bd7b94d1ed5bf
Lalit42224 <lallipandit8171@gmail.com>
2025-05-10 17:47:11 +0000

    coversation

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,12 +1,41 @@
 let score = "33abcd"
-console.log(typeof(score))
+// console.log(typeof(score))
 let scoreNumber = Number(score)
-console.log(scoreNumber)
+// console.log(scoreNumber)
 let isLogged  = true
 let numberbool = Number(isLogged)
-console.log(typeof(numberbool));
-console.log(numberbool);
+// console.log(typeof(numberbool));
+// console.log(numberbool);
 // "33" => 33
 // true =>1;
 // false=>0;
 // "33acvb" => NaN(Not a nUMBER)
+
+//********************************Operations**************************************** */
+// let value = 3
+// let Negvalue = -value
+// // console.log(Negvalue);
+// let str1 ="Lalit"
+// let str2 = " sharma"
+// let str3 = str1 + str2
+// console.log(str3);
+// console.log(1 + 2);
+// console.log(1 - 2);
+// console.log(1 / 2);
+// console.log(1 / 2 );
+// console.log(2 ** 2 );
+// console.log(10 % 2 );
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log(  2 +1 + "2" + 2)
+let gameCounter = 100
+console.log(++gameCounter);
+// console.log(gameCounter++);
+console.log(gameCounter--);
+console.log(--gameCounter);
+
+
+
+
