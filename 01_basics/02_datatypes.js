d094fca8a4772b0bb6c28d8423d10465ba0fab4c
Lalit42224 <lallipandit8171@gmail.com>
2025-05-10 16:34:27 +0000

    datatpes01

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+//alert( 3 + 3)// We r using nodejs,not browser
+console.log(3+3);
+console.log("lalit");
+let name = "lalit"
+let age  = 18
+let isloggedIn = false
+
+// perimtive data types
+// number => 2 to poer 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefineed => abhi value assign nhi hui h 
+// symbol => for unique ness
+
+
+// object
+ console.log(typeof age);
+ console.log(typeof undefined);// undefined
+ console.log(typeof null);// object
+ 
+ 
+
