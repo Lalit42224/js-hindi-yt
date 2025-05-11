f5c4616499e47028a5ad23da2a410c62924f639a
Lalit42224 <lallipandit8171@gmail.com>
2025-05-11 17:28:02 +0000

    stakandheap

---
+++ 01_basics/05_stackandheap.js
@@ -0,0 +1,23 @@
+// stack(peremitive data types), 
+
+let myname  = "lalit sharma"
+let anothername  = "Sumit sharma"
+anothername = "Rachitsharma"
+console.log(anothername);
+console.log(myname);
+
+
+//heap(Non perimitive data types)
+let userone ={
+    email: "lallipandit8171@gmail.com",
+    upi : "mdvnvvjv"
+}
+let userTwo = userone;
+//console.log(userTwo);
+userTwo.email="lkjbuydersdfg"
+console.log(userTwo);
+console.log(userone);
+
+
+
+
