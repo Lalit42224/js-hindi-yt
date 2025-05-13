a3c6deeecb6d52df72b45741faba39c7f56f8dcb
Lalit42224 <lallipandit8171@gmail.com>
2025-05-13 04:35:09 +0000

    stringmanipulation

---
+++ 01_basics/06_string.js
@@ -0,0 +1,35 @@
+const name = "Lalit"
+const repoCount = 50;
+
+console.log(`Hellow my name is ${name} and repoCount is ${repoCount}`)
+
+
+
+const gameName = new String('Lalit-Sharma-com')
+//console.log(gameName);
+//console.log(gameName.__proto__);// to see object
+//console.log(gameName.length);// to find length
+//console.log(gameName.toUpperCase());// to change string into UpperCase
+//console.log(gameName.charAt(4));// to find charcter of give index
+
+//const newString = "  Lalit  "
+//console.log(newString);
+
+//console.log(newString.trim());
+
+const url = "https://lalitshama.com pandit"
+console.log(url.replace(" " , 12));
+console.log(url.split(12));
+
+
+
+
+
+
+
+
+
+
+
+
+
