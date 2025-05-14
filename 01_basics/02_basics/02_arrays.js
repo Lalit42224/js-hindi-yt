03a625f54fa744124dba6530ae853aa239d1ec3d
Lalit42224 <lallipandit8171@gmail.com>
2025-05-14 16:19:50 +0000

    aaray h

---
+++ 01_basics/02_basics/02_arrays.js
@@ -0,0 +1,45 @@
+// arrays two
+const marval_heros = ["thoe","Ironman","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+//marval_heros.push(dc_heros)
+// console.log(marval_heros);
+// console.log(marval_heros[3][1])
+
+
+//// concat///
+//  const newArr = marval_heros.concat(dc_heros)
+// console.log(newArr)
+
+
+
+/////////////////// Spread operators ////////////////////////
+
+
+// const all_new_heros = [...marval_heros , ...dc_heros]
+// console.log(all_new_heros);
+
+
+
+
+/////////////////////////////// ///////////////////////
+
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+
+
+/////////////////////////////////////////////////////////////////////////
+
+console.log(Array.isArray("Lalit"))
+console.log(Array.from("lalit"))
+console.log(Array.from({name: "lalit"}))// interesting
+  let score1=100
+  let score2 = 200
+  let score3  = 300
+  console.log(Array.of(score1,score2,score3));
+  
+
+
+
+
