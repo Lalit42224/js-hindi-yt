5a578337baaeaa30871a5d1c514175c5485a2ee8
Lalit42224 <lallipandit8171@gmail.com>
2025-05-16 06:57:03 +0000

    objects hi h

---
+++ 01_basics/02_basics/04_objects.js
@@ -0,0 +1,30 @@
+// API 
+
+
+
+
+
+const course = {
+    coursename :"js in hindi",
+    price:"999",
+    courseInstructor:"lalit"
+} 
+
+// destruching the obnect element//
+const {courseInstructor:instructor} = course
+console.log(instructor);
+
+//////////////////////////////////////////////////////API//////////////////////////////////////////////////
+
+//// JSOn//////////////////////////
+// {
+//     "name":"lalit"
+//     "coursename":"js in hindi"
+//     "price" : "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
