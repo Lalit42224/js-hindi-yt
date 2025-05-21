b39bf036e5d6767edc148cc72aeef1b476ee1113
Lalit42224 <lallipandit8171@gmail.com>
2025-05-21 03:20:17 +0000

    controlflow

---
+++ conrolflow/one.js
@@ -0,0 +1,31 @@
+// if statement
+//const isUserLoggedIn = true
+
+// if(isUserLoggedIn){
+//     console.log("User yhi h")
+
+// }
+
+// <, > ,<= , >= , == ,!= , == , ===
+
+
+//////////////// 
+// const score = 200
+// if(score >100){
+//    const power  = "fly"
+//    console.log(power);
+//    }
+
+//    console.log(power);  // out of scope 
+   
+
+
+///////////////////// example //////////////////////////
+// const isUserLoggedIn = true
+// const debitcard = true
+// if(isUserLoggedIn && debitcard){
+//     console.log("Allow to buy a course ");
+    
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////
