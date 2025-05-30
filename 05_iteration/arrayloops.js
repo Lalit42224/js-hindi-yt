229916675f9566220ac6d33adfa30ca3995aa09c
Lalit42224 <lallipandit8171@gmail.com>
2025-05-30 17:25:23 +0000

    lops h ye bhi

---
+++ 05_iteration/arrayloops.js
@@ -0,0 +1,51 @@
+// Higher order array loops
+// for of loop
+
+
+// const arr = [1,2,3,4,5]
+// for (const val of arr) {
+//     console.log(val);
+    
+// }
+
+
+
+// const greetings = "hello world"
+//  for (const val of greetings) {
+//     console.log(val);
+    
+    
+//  }
+
+
+//  Maps
+
+// const map = new Map();
+// map.set('USA',"United States of America")
+// map.set('Fr',"france")
+// map.set('IN',"India")
+// map.set('IN',"India")//  No duplicate value is allowd
+// console.log(map);
+//  for (const [key,value] of map) {
+//     console.log(key, ': -', value)
+    
+//  }
+
+
+// loops on object///////
+
+// const myobjects = {
+//     'game':'NFS',
+//     'game2':'Spiderman'
+// }
+// for (const i of myobjects) {
+//     console.log(i);
+    
+    
+// }   objects are not iterable
+
+
+
+
+
+
---
+++ 05_iteration/loopsonobject.js
@@ -0,0 +1,44 @@
+/// lopps on objects
+
+
+
+// const  myobject = {
+//     js: 'javascript',
+//     cpp: 'c++',
+//     rb: 'rubby',
+//     swift : 'swift by apple'
+
+// }
+// for (const key in myobject) {
+//     console.log(`${key} is shortcut for ${myobject[key]} `);
+// }
+
+
+
+
+/////////////// for each loop attached with Array
+
+
+
+
+// const arr = ["js","ruby","java","python","cpp"]
+//   arr.forEach(function (val) {
+//     console.log(val); 
+//   });
+
+
+//////////  ////////  fileter ///
+//   const arr = [1,2,3,4,5,6,7,8,9,10]
+//    const number = arr.filter((num)=>{
+//     return num >4;
+//   }
+// )
+// console.log(number);
+ //////////////////////////////////////////////////////////////
+/// chaning 
+
+// const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+// const newNums = myNumbers.map((num)=> num*10).map((num)=> num+1).filter((num)=> num>=40)
+//  console.log(newNums);
+ 
+
