d3ee8a2897a9ecc6971e9fe063a5721ca0b06223
Lalit42224 <lallipandit8171@gmail.com>
2025-05-30 16:10:27 +0000

    loops

---
+++ 05_iteration/forloop.js
@@ -0,0 +1,31 @@
+// for loop
+for (let index = 0; index < 10; index++) {
+         const element  = index;
+         console.log(element);
+    
+}
+// break;continue;
+
+
+
+// while do loop
+
+// let i = 1;
+// while (true) {
+//     i++;
+//     if(i==10){
+//         break;
+//     }
+//     console.log(i);
+    
+    
+// }
+// let score = 1;
+
+// do{
+//     console.log(`Score is ${score}`);
+//     score++;
+// }
+//     while (score<=10) {
+        
+// }
